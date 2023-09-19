28321d197a59863b7263db395457583d7e7abfbe
Alejandro Martinez <alejandrorappid@gmail.com>
2023-09-18 22:00:27 -0500

    Commit de operadores.js

---
+++ CarpetaHenry/operadores.js
@@ -0,0 +1,35 @@
+// Suma
+let num1 = 5;
+let num2 = 3;
+let resultadoSuma = num1 + num2;
+console.log(resultadoSuma);
+
+// Resta
+let num3 = 5;
+let num4 = 3;
+let resultadoResta = num1 - num2;
+console.log(resultadoResta);
+
+// Resta
+let num5 = 5;
+let num6 = 3;
+let resultadoMultiplicacion = num5 * num6;
+console.log(resultadoMultiplicacion);
+
+// Division
+let num7 = 4;
+let num8 = 2;
+let resultadoDivision = num7 / num8;
+console.log(resultadoDivision);
+
+// Resto o Modulo de la division
+let num9 = 4;
+let num10 = 2;
+let resultadoModulo = num5 % num6;
+console.log(resultadoModulo);
+
+// Concatenacion
+let nombre = 'Alejandro ';
+let apellido = 'Martinez';
+
+console.log(nombre + apellido);
