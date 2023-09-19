79bbc60e74073ff3256b2b49ce4d8ab1052aa83b
Alejandro Martinez <alejandrorappid@gmail.com>
2023-09-18 21:57:58 -0500

    Commit de variables.js

---
+++ CarpetaHenry/variables.js
@@ -0,0 +1,14 @@
+// Variables JavaScript
+// String o cadenas de texto
+let nombre = 'Alejandro';
+// Numeros, enteros, negativos, decimales
+let entero = 36;
+let negativo = -10;
+let decimal = 11.10;
+// Bolleanos
+let verdadero = true;
+let falso = false;
+// Undefined (variable no tiene definido un valor)
+let indefinado;
+// Null
+let nulo = null;
